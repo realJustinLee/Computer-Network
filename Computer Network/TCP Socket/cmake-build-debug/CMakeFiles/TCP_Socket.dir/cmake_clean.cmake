cabcea4321e1d6ee27c991468a45686fc22d1ba8
李欣 <JustinDellAdam@live.com>
2017-10-08 21:03:01 +0800

    TCP_SOCKET

---
+++ Computer Network/TCP Socket/cmake-build-debug/CMakeFiles/TCP_Socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP_Socket.dir/srcTcpSocket.c.o"
+  "TCP_Socket.pdb"
+  "TCP_Socket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCP_Socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Computer Network/TCP Socket/cmake-build-debug/CMakeFiles/TCP_Socket.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TCP_Socket.
+# This may be replaced when dependencies are built.
